fee5eac40c6da6e2a790c66dcaabc0f371601138
priyanshu-iottive <priyanshu.agrawal@iottive.com>
2026-03-06 18:17:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+const morgan = require("morgan")
+
+app.use(morgan("dev"))
+
+const PORT = process.env.PORT
+
+app.get("/test", (req, res) => {
+    res.status(200).json({ message: "This is Message" })
+})
+
+app.listen(PORT, () => {
+    console.log(`localhost:${PORT}`)
+})
