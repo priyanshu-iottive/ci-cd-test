502eba063d7606809ea8e2fb224ab4d6e68f68d9
priyanshu-iottive <priyanshu.agrawal@iottive.com>
2026-03-09 11:32:17 +0530

    initial commit after cicd

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT
 app.get("/test", (req, res) => {
     res.status(200).json({ message: "This is Message" })
 })
+app.get("/test-another", (req, res) => {
+    res.status(200).json({ message: "This is Message" })
+})
 
 app.listen(PORT, () => {
     console.log(`localhost:${PORT}`)
